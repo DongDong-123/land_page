7bea10c876e9c15ebc9e00ee8aa9cdba27394f53
dongdong <zisehaiyang04@163.com>
2019-09-19 15:19:00 +0800

    增加向下滚动距离

--- land_page_for_test.py
+++ land_page_for_test.py
@@ -57,7 +57,7 @@ class test_land_page:
         if page == "land" and buttom == 1:
             self.driver.refresh()
             time.sleep(self.stop_time)
-            scroll_js = "window.scrollTo(0,500)"
+            scroll_js = "window.scrollTo(0,1000)"
             self.driver.execute_script(scroll_js)
             send_phone = self.make_phone()
             self.driver.find_element_by_xpath(".//input[@id='tan2-phone']").send_keys(send_phone)
